93105a583f03a123675df87a7b82336858483b60
marwenmaalawi <mohamedmarwen.maalawi@gmail.com>
2025-03-04 06:08:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ export * from "./cards/index.js";
 import express from "express";
 const app = express();
 
+import topLangsRoute from "../api/top-langs.js"; 
+app.use("/api/top-langs", topLangsRoute);
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
