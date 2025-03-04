3a6baa27c1a268956eadb1704ae3f3ab569ef6ed
marwenmaalawi <mohamedmarwen.maalawi@gmail.com>
2025-03-04 05:44:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 export * from "./common/index.js";
 export * from "./cards/index.js";
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
