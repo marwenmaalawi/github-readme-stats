34e4f3194e21c78f253d87a750f720e4410c335f
marwenmaalawi <mohamedmarwen.maalawi@gmail.com>
2025-03-04 07:15:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 export * from "./common/index.js";
 export * from "./cards/index.js";
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("✅ GitHub Readme Stats API is running!");
-});
-
-
-import topLangsRoute from "../api/top-langs.js"; 
-app.use("/api/top-langs", topLangsRoute);
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
